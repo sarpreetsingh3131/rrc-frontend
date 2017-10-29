206bc82507ad51947ad0aba2ad56ca5f7439dd3e
sarpreetsingh3131 <sunnybuttar31@gmail.com>
2017-10-29 20:52:47 +0100

    made header and rugs component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import React from 'react'
+import { Header } from './components/header'
+import { Rugs } from './components/rugs'
 
 export default class App extends React.Component {
-  render () {
+  render() {
     return (
-      <h1>Rotorua Rug Co.</h1>
+      <div>
+        <Header />
+        <main className="w-100 cf helvetica dark-gray bg-white pa3 pa4-m pa5-l mw9 center">
+          <Rugs /> 
+        </main>
+      </div>
     )
   }
 }
